ec6d22121a78808b1bac773b7b380f205d1ecd72
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-04 14:58:29 -0700

    alter sql statement

--- gigs.sql
+++ gigs.sql
@@ -18,77 +18,46 @@
 SET foreign_key_checks = 0; #turn off constraints temporarily
 
 DROP TABLE IF EXISTS Gig;
-DROP TABLE IF EXISTS Gig_responses;
 
+CREATE TABLE Company(
+CompanyID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+User varchar(255),
+InputType ENUM('checkbox','radio','select','text') DEFAULT 'select',
+CompanyName varchar(100) NOT NULL,
+CompanyAddress varchar(85) NOT NULL,
+City varchar(40) NOT NULL,
+ST varchar(25) NOT NULL DEFAULT "WA",
+ZipCode MEDIUMINT UNSIGNED NOT NULL,
+CoPhone varchar(25) NOT NULL,
+Website varchar(100) NOT NULL,
+DateAdded DATETIME,
+PRIMARY KEY (CompanyID)
+FOREIGN KEY (GigID) REFERENCES ContactID (CompanyID)
+FOREIGN KEY (ContactID) REFERENCES GigID(CompanyID)
+)ENGINE=INNODB;
 
+CREATE TABEL Contact(
+ContactID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+FirstName varchar(30) NOT NULL,
+LastName varchar(30) NOT NULL,
+Email varchar(75) NULL,
+Phone varchar(25) NOT NULL,
+PRIMARY KEY (CompanyID)
+FOREIGN KEY (GigID) REFERENCES ContactID (CompanyID)
+FOREIGN KEY (ContactID) REFERENCES GigID(CompanyID)
+)ENGINE=INNODB;
 
 CREATE TABLE Gig(
 GigID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-User TEXT DEFAULT '',
-InputType ENUM('checkbox','radio','select','text') DEFAULT 'select',
-CompanyName TEXT DEFAULT '',
-CompanyAddress TEXT DEFAULT '',
-City TEXT DEFAULT '',
-ST TEXT DEFAULT '',
-ZipCode TEXT DEFAULT '',
-CoPhone TEXT DEFAULT '',
-Website TEXT DEFAULT '',
-DateAdded DATETIME,
-
-FirstName TEXT DEFAULT '',
-LastName TEXT DEFAULT '',
-Email TEXT DEFAULT '',
-Phone TEXT DEFAULT '',
-
-WorkExper TEXT DEFAULT '',
-InterviewLoc TEXT DEFAULT '',
-ApptTime DATETIME,
-GigDesc TEXT DEFAULT '',
-ProDetail TEXT DEFAULT '',
-PayRate TEXT DEFAULT '',
+WorkExper varchar(500) NOT NULL,
+InterviewLoc varchar(255) NOT NULL,
+ApptTime varchar(255) NOT NULL,
+GigDesc varchar(500) NOT NULL,
+ProDetail varchar(350) NULL,
+PayRate varchar(50) NOT NULL,
 LastUpdated TIMESTAMP DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (GigID)
-)ENGINE=INNODB;
+PRIMARY KEY (CompanyID)
+FOREIGN KEY (GigID) REFERENCES ContactID(CompanyID)
+FOREIGN KEY (ContactID) REFERENCES GigID(CompanyID)
+)ENGINE=INNODB; #INNODB allows creation table w/the same name in another db, table names will not collide
 
-INSERT INTO Gig VALUES (NULL,'I am a','select','Company Name','Company Address','City','State','Zip Code','Company Phone','Website','First Name','Last Name','Email','Phone','Work Experience','Interview Location','Appointment Time','Gig Description','Project Details','Pay Rate',NOW(),NOW());
-
-
-CREATE TABLE Gig_responses(
-GigResponsesID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-GigID INT UNSIGNED DEFAULT 0,
-UserResponses TEXT DEFAULT '',
-
-CompanyNameResponses TEXT DEFAULT '',
-CompanyAddressResponses TEXT DEFAULT '',
-CityResponses TEXT DEFAULT '',
-STResponses TEXT DEFAULT '',
-ZipCodeResponses TEXT DEFAULT '',
-CoPhoneResponses TEXT DEFAULT '',
-WebsiteResponses TEXT DEFAULT '',
-DateAdded DATETIME,
-
-FirstNameResponses TEXT DEFAULT '',
-LastNameResponses TEXT DEFAULT '',
-EmailResponses TEXT DEFAULT '',
-PhoneResponses TEXT DEFAULT '',
-
-WorkExperResponses TEXT DEFAULT '',
-InterviewLocResponses TEXT DEFAULT '',
-ApptTimeResponses DATETIME,
-GigDescResponses TEXT DEFAULT '',
-ProjectDeResponses TEXT DEFAULT '',
-PayRateResponses TEXT DEFAULT '',
-
-DateAdded DATETIME,
-LastUpdated TIMESTAMP DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
-Status INT DEFAULT 0,
-PRIMARY KEY (GigResponsesID),
-INDEX Gig_index(GigID),
-FOREIGN KEY (GigID) REFERENCES Gig(GigID) ON DELETE CASCADE
-)ENGINE=INNODB;
-
-INSERT INTO Gig_responses values (NULL,1,'Student','','','','',NOW(),NOW(),0);
-INSERT INTO Gig_responses values (NULL,1,'Alumni','','','','',NOW(),NOW(),0);
-INSERT INTO Gig_responses values (NULL,1,'Staff','','','','',NOW(),NOW(),0);
-INSERT INTO Gig_responses values (NULL,1,'Employer','','','','',NOW(),NOW(),0);
-INSERT INTO Gig_responses values (NULL,1,'Agency','','','','',NOW(),NOW(),0);
